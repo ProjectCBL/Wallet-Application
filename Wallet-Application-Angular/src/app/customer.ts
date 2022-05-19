82c722f703d605ad9d3b72d5e615218c749d2d72
ProjectCBL <projectxcbl@gmail.com>
2022-05-18 20:37:29 -0400

    Create interfaces to hold response data

---
+++ Wallet-Application-Angular/src/app/customer.ts
@@ -0,0 +1,12 @@
+export class Customer{
+
+    public customerId:number = 0;
+    public userName:String = "";
+    public password:String = "";
+    public email:String = "";
+    public firstName:String = "";
+    public lastName:String = "";
+    public walletBalance:number = 0;
+    public savingBalance:number = 0;
+
+}
