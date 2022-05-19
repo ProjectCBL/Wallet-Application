fa6f126d1b1600146466e4219a95220ef4b3953c
ProjectCBL <projectxcbl@gmail.com>
2022-05-18 20:39:28 -0400

    Http client module

--- Wallet-Application-Angular/src/app/app.module.ts
+++ Wallet-Application-Angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AccessComponent } from './access/access.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -39,7 +40,7 @@ const routes: Routes = [
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule,
+		HttpClientModule,
 		FormsModule,
 		[RouterModule.forRoot(routes)]
 	],
