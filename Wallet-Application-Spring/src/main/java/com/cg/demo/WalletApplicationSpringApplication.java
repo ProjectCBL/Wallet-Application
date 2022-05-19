587eb9dda4560406eff1f24ed6be6627d3aecb13
ProjectCBL <projectxcbl@gmail.com>
2022-05-19 01:48:45 -0400

    Add cross origin

--- Wallet-Application-Spring/src/main/java/com/cg/demo/WalletApplicationSpringApplication.java
+++ Wallet-Application-Spring/src/main/java/com/cg/demo/WalletApplicationSpringApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.cg.wallet.controller.WalletAppController;
 import com.cg.wallet.service.ApplicationService;
 import com.cg.wallet.service.ApplicationServiceImpl;
 
+@CrossOrigin
 @SpringBootApplication
 @EnableJpaRepositories("com.cg.wallet.repository")
 @EntityScan("com.cg.wallet.entities") 
