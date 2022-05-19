427393e4c8619c5566d4da3df41ab46f2cab2adb
ProjectCBL <projectxcbl@gmail.com>
2022-05-19 11:52:39 -0400

    Implemented service class

--- Wallet-Application-Angular/src/app/app.service.ts
+++ Wallet-Application-Angular/src/app/app.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { TransferState } from "@angular/platform-browser";
 import { Observable } from "rxjs";
 import { Customer } from "./customer";
 import { Transaction } from "./transaction";
@@ -12,25 +13,63 @@ export class AppService {
 
     }
 
-    public login(username: string, password: string): Observable<Customer> {
+    public validateLogin(username: string, password: string): Observable<Customer> {
         return this.httpClient.post<Customer>('http://localhost:8080/wallet-application/validateLogin', {
-            "username":username,
-            "password":password
+            "username": username,
+            "password": password
         });
     }
 
-    public register(username: string, password: string, email: string, firstname: string, lastname: string): Observable<number>{
+    public register(username: string, password: string, email: string, firstname: string, lastname: string): Observable<number> {
         return this.httpClient.post<number>('http://localhost:8080/wallet-application/registerCustomer', {
-            "userName":username,
-            "password":password,
-            "email":email,
-            "firstName":firstname,
-            "lastName":lastname
+            "userName": username,
+            "password": password,
+            "email": email,
+            "firstName": firstname,
+            "lastName": lastname
         });
     }
 
-    /*public addMoneyToWallet(accountId:number, amount:number):Observable<Transaction>{
+    public addMoneyToWallet(accountId: number, amount: number): Observable<Transaction> {
+        return this.httpClient.post<Transaction>('http://localhost:8080/wallet-application/addMoneyToWallet', {
+            "accountId":accountId,
+            "amount":amount 
+        });
+    }
 
-    }*/
+    public depositMoneyToSaving(accountId: number, amount: number): Observable<Transaction>{
+        return this.httpClient.post<Transaction>('http://localhost:8080/wallet-application/depositMoneyToSavings', {
+            "accountId":accountId,
+            "amount":amount
+        });
+    }
+
+    public withdrawMoney(accountId: number, amount: number, source: string): Observable<Transaction>{
+        return this.httpClient.post<Transaction>('http://localhost:8080/wallet-application/withdrawMoney', {
+            "accountId":accountId,
+            "amount":amount,
+            "source":source
+        });
+    }
+
+    public transferMoney(accountId:number, source:string, destination:string, amount:number): Observable<Transaction>{
+        return this.httpClient.post<Transaction>('http://localhost:8080/wallet-application/transferMoney',{
+            "accountId":accountId,
+            "source":source,
+            "destination":destination,
+            "amount":amount
+        });
+    }
+
+    public getLastTenTransactions(accountId: number): Observable<Transaction[]>{
+        return this.httpClient.get<Transaction[]>('http://localhost:8080/wallet-application/getLastTenTransactions?id=' + accountId);
+    }
+
+    public searchForTransactionAtDate(accountId: number, transactionDate: string): Observable<Transaction[]>{
+        return this.httpClient.post<Transaction[]>('http://localhost:8080/wallet-application/searchForTransactionAtDate', {
+            "accountId":accountId,
+            "transactionDate":transactionDate
+        });
+    }
 
 }
