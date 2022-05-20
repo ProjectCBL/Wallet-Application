5747e7dddbf80ad3517bceecd81fecee8d9f1274
ProjectCBL <projectxcbl@gmail.com>
2022-05-20 13:24:22 -0400

    Constructed dashboard layout

--- Wallet-Application-Angular/src/app/dashboard/dashboard.component.ts
+++ Wallet-Application-Angular/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as $ from 'jquery';
+import { AppService } from '../app.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+	walletAction:number=0
+	savingAction:number=0
 
-  ngOnInit(): void {
-  }
+	accountId:number = 0;
+	firstname:string = ""
+	walletBalance:number = 0;
+	savingBalance:number = 0;
+
+	constructor(private router: Router, private appService: AppService) { 
+		this.accountId = localStorage.getItem("customerId") as unknown as number;
+		this.firstname = localStorage.getItem("firstName") as unknown as string;
+		this.walletBalance = localStorage.getItem("walletBalance") as unknown as number;
+		this.savingBalance = localStorage.getItem("savingBalance") as unknown as number;
+	}
+
+	ngOnInit(): void {
+	}
+
+	public updateWalletBalance(){
+		this.walletBalance = localStorage.getItem("walletBalance") as unknown as number;
+	}
+
+	public logout(){
+		localStorage.clear();
+		this.router.navigate(['']);
+	}
 
 }
