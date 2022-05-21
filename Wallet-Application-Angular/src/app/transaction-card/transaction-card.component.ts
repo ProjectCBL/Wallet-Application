09ebfd2d9aa15f275649c888ea1a91fa66693935
ProjectCBL <projectxcbl@gmail.com>
2022-05-21 06:49:00 -0400

    Removed hr tag for reusablitiy

--- Wallet-Application-Angular/src/app/transaction-card/transaction-card.component.ts
+++ Wallet-Application-Angular/src/app/transaction-card/transaction-card.component.ts
@@ -1,26 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-transaction-card',
-  templateUrl: './transaction-card.component.html',
-  styleUrls: ['./transaction-card.component.css']
+	selector: 'app-transaction-card',
+	templateUrl: './transaction-card.component.html',
+	styleUrls: ['./transaction-card.component.css']
 })
 export class TransactionCardComponent implements OnInit {
 
-  @Input() type:String = ""
-	@Input() dateOfTransaction:string = ""
-	@Input() timeOfTransaction:string = ""
-	@Input() walletBalanceBefore:number = 0;
-	@Input() savingBalanceBefore:number = 0;
-	@Input() walletBalanceAfter:number = 0;
-	@Input() savingBalanceAfter:number = 0;
-	@Input() source:string = '';
-	@Input() destination:string = '';
-	@Input() amount:number = 0;
+	@Input() type: String = ""
+	@Input() dateOfTransaction: string = ""
+	@Input() timeOfTransaction: string = ""
+	@Input() walletBalanceBefore: number = 0;
+	@Input() savingBalanceBefore: number = 0;
+	@Input() walletBalanceAfter: number = 0;
+	@Input() savingBalanceAfter: number = 0;
+	@Input() source: string = '';
+	@Input() destination: string = '';
+	@Input() amount: number = 0;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
