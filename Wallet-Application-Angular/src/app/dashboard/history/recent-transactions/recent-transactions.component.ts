c7a8c4203983f7577f42b7e3d9ac43c04e5ce3af
ProjectCBL <projectxcbl@gmail.com>
2022-05-21 13:54:20 -0400

    Implemented 'get last ten items feature'

--- Wallet-Application-Angular/src/app/dashboard/history/recent-transactions/recent-transactions.component.ts
+++ Wallet-Application-Angular/src/app/dashboard/history/recent-transactions/recent-transactions.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Transaction } from 'src/app/transaction';
+import * as $ from 'jquery';
+import { AppService } from 'src/app/app.service';
 
 @Component({
-  selector: 'app-recent-transactions',
-  templateUrl: './recent-transactions.component.html',
-  styleUrls: ['./recent-transactions.component.css']
+	selector: 'app-recent-transactions',
+	templateUrl: './recent-transactions.component.html',
+	styleUrls: ['./recent-transactions.component.css']
 })
 export class RecentTransactionsComponent implements OnInit {
 
-  constructor() { }
+	showError:boolean = false;
+	currentIndex:number = 0;
+	dateOfTransaction:string = "";
+	timeOfTransaction:string = "";
+	transactions:Transaction[] = [];
 
-  ngOnInit(): void {
-  }
+	constructor(private appService: AppService) { }
+
+	ngOnInit(): void {
+		this.getRecentTransactions();
+		console.log(this.transactions);
+	}
+
+	getRecentTransactions(){
+
+		const accountId = localStorage.getItem('customerId') as unknown as number; 
+
+		this.appService.getLastTenTransactions(accountId).subscribe((response:Transaction[])=>{
+			const len = response.length;
+			if(response != null && len != 0){
+				this.transactions.push(...response);
+				this.currentIndex = 0;
+
+				let date = new Date(String(this.transactions[this.currentIndex].dateOfTransaction));
+				this.dateOfTransaction = date.toLocaleDateString();
+				this.timeOfTransaction = date.toLocaleTimeString();
+
+				this.showError = false;
+			}
+			else if(len == 0){
+				this.transactions.push(...response);
+				return;
+			}
+		},
+		(error)=>{
+			this.showError = true;
+		});
+
+	}
+
+	changeActive(newActiveIndex:number){
+		this.currentIndex = newActiveIndex;
+		let date = new Date(String(this.transactions[this.currentIndex].dateOfTransaction));
+		this.dateOfTransaction = date.toLocaleDateString();
+		this.timeOfTransaction = date.toLocaleTimeString();
+	}
 
 }
